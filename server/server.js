927e82a3a565f534e6df53e7668878a728b226d8
Hien Tran <aznketchup@yahoo.com>
2016-08-04 19:10:23 -0700

    Initialize WebSockets for server and update package.json.
    -Initialize websockets and connection for socket listeners.
    -Update package.json with react,react-dom,react-redux,socket.io, and socket.io-client.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const http = require('http');
 const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/../client/public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Socket listeners
+io.on('connection', (socket)=>{
+  //Add listeners here
+});
 
 // serve index.html for rest
 app.get('*', (req, res)=>{
@@ -16,6 +23,6 @@ app.get('*', (req, res)=>{
 });
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, function() {
+server.listen(port, ()=>{
   console.log('Listening on port', port);
 });
