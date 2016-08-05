428d9b4e677432554ff260fed1df1d94db5a666e
andrew-heath <aheath42@gmail.com>
2016-08-05 11:56:05 -0700

    Added webpack to build bundle, include babel for compiling ES6/7/React-Redux, update gitignore to ignore bundle folder

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const PATHS = {
+  compiled: path.join(__dirname, 'client/public/scripts'),
+  src: path.join(__dirname, 'client/public/index.js')
+};
+
+module.exports = {
+  entry: PATHS.src,
+  output: {
+    path: PATHS.compiled,
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /scripts/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolveLoaders: {
+    modulesDirectories: ['node_modules']
+  },
+  resolve: { // in import statements default to these file types if none specified
+    extensions: ['', '.js', '.jsx'],
+    modulesDirectories: ['node_modules']
+  },
+  devServer: {
+    contentBase: PATHS.compiled,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
