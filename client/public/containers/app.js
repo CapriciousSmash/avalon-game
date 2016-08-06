06d4db272fac4392d1298ac3a64042cdf0e2f7fb
cptran777 <cptran14@gmail.com>
2016-08-06 13:55:55 -0700

    basic skeletal structure of files set up.

---
+++ client/public/containers/app.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Router, Route, Link } from 'react-router';
+
+// Redux-related imports
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as Actions from '../actions';
+
+// CSS-related import
+import '../styles/stylesheet.css';
+
+// The app class acts as the main container to store the entirety
+// of what the user will see as the web application
+class App extends React.Component {
+}
+
+// Passes data to the container from the store. Makes the result of
+// reducers available to the containers as props
+function mapStateToProps(state) {
+  return {};
+}
+
+// Passes data from container to the store. Provides ability for 
+// container to tell the store that it needs to change. Adds action 
+// creators to container as props
+function mapDispatchToProps(dispatch) {
+  return {
+  	actions: bindActionCreators(Actions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
