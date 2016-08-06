13b2527dab28e4ec5114a34b72712218c1f39c69
cptran777 <cptran14@gmail.com>
2016-08-06 14:19:22 -0700

    modify for name change.

--- client/public/index.js
+++ client/public/index.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/app';
+import App from './containers/App';
 import { Provider } from 'react-redux';
 import configureStore from './store/configStore';
 
