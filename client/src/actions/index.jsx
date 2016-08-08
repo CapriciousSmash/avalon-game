66645fb465d61c00c07c415901489d31f31abeea
cptran777 <cptran14@gmail.com>
2016-08-08 16:00:37 -0700

    update game-info

--- client/src/actions/index.jsx
+++ client/src/actions/index.jsx
@@ -1,2 +1,13 @@
 export const LOGGED_IN = 'LOGGED_IN';
 export const LOGGED_OUT = 'LOGGED_OUT';
+
+// Actions for Game Info page:
+export const CHANGE_INFO = 'CHANGE_INFO';
+
+// Action Creators for Game Info page: 
+export function newInfoSection(pageName = 'GETTING STARTED') {
+  return {
+  	type: CHANGE_INFO,
+  	activeSection: pageName
+  };
+}
