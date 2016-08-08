9bd62efc1ae5c5362923d14f1be8d07b6fa40284
andrew-heath <aheath42@gmail.com>
2016-08-08 15:44:58 -0700

    Implement functions to be used once written.

--- server/logic/logic-main.js
+++ server/logic/logic-main.js
@@ -1,4 +1,11 @@
 // require all files in modules
+var assignRoles = require('./modules/assignRoles.js');
+var chooseParty = require('./modules/chooseParty.js');
+var voteOnParty = require('./modules/voteOnParty.js');
+var decideQuest = require('./modules/decideQuest.js');
+var identifyMerlin = require('./modules/identifyMerlin.js');
+var gameOver = require('./modules/gameOver.js');
+
 // require all other node_modules needed
 // export main loop function
 
@@ -6,38 +13,38 @@
 module.exports.gameLoop = function(memcache) {
   // keep track of current game stage and if the game is over or not
   var stage = 'roles';
-  var gameOver = false;
+  var isGameOver = false;
 
   // as long as gameOver is false, keep running the loop
-  while (!gameOver) {
+  while (!isGameOver) {
     // based on stage, run the code to handle that stage
     // stage and gameOver will be passed into functions to be mutated
     // as needed
 
     // Assigning random roles
     if (stage === 'roles') {
-      // stage = func(memcache)
+      stage = assignRoles(memcache);
     }
     // Choose the party to go on a quest
     if (stage === 'party') {
-      // stage = func(memcache)
+      stage = chooseParty(memcache);
     }
     // Vote for if the party is viable
     if (stage === 'vote') {
-      // stage = func(memcache)
+      stage = voteOnParty(memcache);
     }
     // Players decide to succeed or fail a quest
     if (stage === 'quest') {
-      // stage = func(memcache)
+      stage = decideQuest(memcache);
     }
     // Assassin guesses who Merlin is
     if (stage === 'identifyMerlin') {
-      // stage = func(memcache)
+      stage = identifyMerlin(memcache);
     }
     // Game Over, handle any closing issues
     if (stage === 'gameOver') {
-      // stage = func(memcache)
-      gameOver = true;
+      stage = gameOver(memcache);
+      isGameOver = true;
     }
 
   }
