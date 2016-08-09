d835851b904f278c904836a12599bd1587f29e88
cptran777 <cptran14@gmail.com>
2016-08-08 20:26:30 -0700

    InfoWrapper provides props to info and allows Info and InfoBar to change state to navigate between nested components within Info.

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,8 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 // Component Imports
 import App from './containers/App';
 import NavBar from './containers/NavBar';
+import InfoWrapper from './containers/InfoWrapper'
 import Main from './components/Main';
-import Info from './components/Info';
 import Stats from './components/Stats';
 import Login from './components/Login';
 
@@ -21,7 +21,7 @@ ReactDOM.render(
     <Router history={browserHistory}>
       <Route path='/' component={App}>
         <Route path='main' component={Main}></Route>
-        <Route path='gameinfo' component={Info}></Route>
+        <Route path='gameinfo' component={InfoWrapper}></Route>
         <Route path='signin' component={Login}></Route>
       </Route>
     </Router>
