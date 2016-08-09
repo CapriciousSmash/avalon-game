b458ea3a1e6138ce5bc61fdd4b1d92daf1537ce1
andrew-heath <aheath42@gmail.com>
2016-08-08 19:49:38 -0700

    Setup outline for voteOnParty

--- server/logic/modules/voteOnParty.js
+++ server/logic/modules/voteOnParty.js
@@ -1,11 +1,21 @@
 module.exports.voteOnParty = function(memcache) {
-  if(/* Party is rejected */) {
-    if (/* Veto count exceeds 5 */) {
+
+  // pause game for 1 minute
+    // Send back countdown?
+
+  // check votes to see if party is rejected
+  if(/* Party is rejected accepts < rejects*/) {
+    if (/* Veto count is already 5 */) {
+      // set minions to winners
       return 'gameOver';
     } else {
+      // increase veto count
+      // assign next party leader
+      // empty party selection
       return 'party';
     }
   } else {
+    // set veto count to 5
     return 'quest';
   }
 }
