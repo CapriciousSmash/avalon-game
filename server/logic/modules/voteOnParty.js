e8882f8b2571af9222eaf3d06e5bcb781ee9905d
andrew-heath <aheath42@gmail.com>
2016-08-08 21:36:56 -0700

    Fix code to account for impossibility of desired results.  Will change for nested setIntervals.

--- server/logic/modules/voteOnParty.js
+++ server/logic/modules/voteOnParty.js
@@ -1,21 +1,21 @@
 module.exports.voteOnParty = function(memcache) {
-
-  // pause game for 1 minute
-    // Send back countdown?
-
+  // handle vote counting
   // check votes to see if party is rejected
   if(/* Party is rejected accepts < rejects*/) {
     if (/* Veto count is already 5 */) {
       // set minions to winners
-      return 'gameOver';
+      // return 'gameOver';
+      // setTimout 15 sec for gameOver
     } else {
       // increase veto count
       // assign next party leader
       // empty party selection
-      return 'party';
+      // return 'party';
+      // setTimout 5m for party
     }
   } else {
     // set veto count to 5
-    return 'quest';
+    // return 'quest';
+    // setTimeout 15 sec for quest
   }
 }
