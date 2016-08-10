9ae69bf915f43ca84fa3487ab263e3372327eda8
cptran777 <cptran14@gmail.com>
2016-08-10 16:41:37 -0700

    update socket signaling md and completed decide quest preliminary code.

--- docs/Socket Signals.md
+++ docs/Socket Signals.md
@@ -7,3 +7,4 @@ SIGNAL 			EVENT
 'startVote'		Informs players voting has begun and gives option to accept/reject
 'resolveVote'	Reveals results of player voting to accept/reject party and begin next phase
 'startQuest'	Quest is starting and informs players that the party members are voting
+'resolveQuest'	Inform players that the quest is over and the results of the quest
--- server/logic/modules/decideQuest.js
+++ server/logic/modules/decideQuest.js
@@ -1,3 +1,6 @@
+var gameEnd = require('./gameOver').gameEnd;
+var chooseParty = require('./chooseParty').chooseParty;
+
 module.exports.startQuest = function(memcache, socket) {
   // Information needed from memcache
   // - Current party composition
@@ -20,30 +23,81 @@ module.exports.startQuest = function(memcache, socket) {
 };
 
 var resolveQuest = function(memcache, socket) {
+  var gamePhase = '<-- FROM MEMCACHE -->';
+  // If the current game phase isn't 'QUEST', fizzle
+  if (gamePhase !== 'QUEST') {
+    return;
+  }
+
   // Information needed from memcache
   // - Current party composition
-  // - Results from previous quests (successes and failures)
+  var partyMembers = ['<-- FROM MEMCACHE -->'];
+  // - Total number of players
+  var totalPlayers = '<-- FROM MEMCACHE -->';
+  // - Results from previous quests (successes and failures
+  var numSuccess = '<-- FROM MEMCACHE -->';
+  var numFailures = '<-- FROM MEMCACHE -->';
+  var currentQuest = numSuccess + numFailures + 1;
   // - Current quest voting results 
-
-  // TODO: If the current game phase isn't 'QUEST', fizzle
+  var playerVotes = ['<-- FROM MEMCACHE -->'];
 
   // TODO: Determine quest success or failure based on voting results
+  var successVotes = '<-- SEARCH FROM PLAYERVOTES -->';
+  var failureVotes = '<-- SEARCH FROM PLAYERVOTES -->';
 
-  // if (/* Quest succeeded */) {
-  //   // TODO: Inform (signal websocket) players that the quest succeeded
-  //   if (/* 3 quests have succeeded */) {
-  //     // TODO: Set timer for gameEnd
-  //   } else /* Less than 3 quests succeeded */ {
-  //     // TODO: Set timer for chooseParty
-  //   }
-  // } else /* Quest failed */ {
-  //   // TODO: Inform (signal) players that the quest has failed
-  //   if (/* 3 Quests have failed */) {
-  //     // TODO: Set timer for gameEnd with minion victory
-  //   } else /* Less than 3 quests have failed */ {
-  //     // TODO: Set timer for chooseParty
-  //   }
-  // }
+  var requiredVotesToFail = currentQuest === 4 && totalPlayers >= 7 ? 2 : 1;
+  var questSucceeded = failureVotes < requiredVotesToFail ? true : false;
+
+  if (questSucceeded) {
+    // TODO: Inform (signal websocket) players that the quest succeeded
+    socket.emit('resolveQuest', {
+      result: 'success',
+      numSuccess: numSuccess + 1,
+      numFailures,
+      successVotes,
+      failureVotes
+    });
+    if (++numSuccess >= 3) {
+      // TODO: Set winners to knights in the memcache
+
+      //  Set timer for gameEnd
+      setTimeout(function() {
+        gameEnd(memcache, socket);
+      }, 5000000);
+
+    } else /* Less than 3 quests succeeded */ {
+      // TODO: Increase the total number of successes in memcache
+
+      // TODO: Set timer for chooseParty
+      setTimeout(function() {
+        chooseParty(memcache,socket);
+      }, 5000000);
+    }
+  } else /* Quest failed */ {
+    // TODO: Inform (signal) players that the quest has failed
+    socket.emit('resolveQuest', {
+      result: 'failure',
+      numSuccess,
+      numFailures: numFailures + 1,
+      successVotes,
+      failureVotes
+    });
+    if (++numFailures >= 3) {
+      // TODO: Set winners to minions in the memcache
+
+      // TODO: Set timer for gameEnd with minion victory
+      setTimeout(function() {
+        gameEnd(memcache, socket);
+      }, 5000000);
+    } else /* Less than 3 quests have failed */ {
+      // TODO: Increase total number of failures in memcache
+
+      // TODO: Set timer for chooseParty
+      setTimeout(function() {
+        chooseParty(memcache, socket);
+      }, 5000000);
+    }
+  }
 };
 
 module.exports.resolveQuest = resolveQuest;
