f1deb2d8c865c647b7b894c6ae8df4c3fe271528
andrew-heath <aheath42@gmail.com>
2016-08-11 16:04:51 -0700

    Setup outline to turn redis server from singular to class that creates multiple

--- server/db/redis.js
+++ server/db/redis.js
@@ -1,8 +1,22 @@
-//Initialize Redis either with heroku if .env included or local redis
-const db = require('redis').createClient(process.env.REDIS_URL);
+var Promise = requre('bluebird');
 
-db.on('error', function(err) {
-  console.log('ERROR' + err);
-});
 
-module.exports = db;
+// Class that creates new DB instances and client connections for each game
+// Initialize Redis either with heroku if .env included or local redis
+var makeCache = function(gameNumber) {
+  this.gameNumber = gameNumber;
+  this.db = require('redis').createClient(process.env.REDIS_URL);
+  this.client = redis.createClient();
+
+  this.client.on('error', function(err) {
+    console.log('ERROR' + err);
+  });
+
+
+}
+
+makeCache.prototype.function() {
+
+};
+
+module.exports = new makeCache();
