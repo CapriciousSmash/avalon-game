df6f0816cef741fff1e06a57c22b8551f33207f6
andrew-heath <aheath42@gmail.com>
2016-08-10 17:07:47 -0700

    Add getScore method to db

--- server/db/controller/index.js
+++ server/db/controller/index.js
@@ -1,9 +1,16 @@
-var model = require('../sequelize.js');
+var sequelize = require('../sequelize.js').sequelize;
+var User = requre('../sequelize.js').User;
 var Promise = require('bluebird');
 var bcrypt = require('bcrypt-nodejs');
 
 module.exports = {
-  // createUser
-    // 
-  // authorizeUsers
+  getScore: function(id) {
+    return User.find({id: id})
+    .then(function(user) {
+      var score = user.score;
+      var games = user.games;
+
+      return [score, games].slice();
+    });
+  }
 }
