f0b0c5bc70b9ff545882514dc9d09be4ef51a0dd
cptran777 <cptran14@gmail.com>
2016-08-12 11:18:10 -0700

    chooseParty logic completed.

--- server/logic/modules/chooseParty.js
+++ server/logic/modules/chooseParty.js
@@ -2,83 +2,85 @@ var voteOnParty = require('./voteOnParty').voteOnParty;
 
 // The current party leader must choose the requisite number of players to go on a Quest
 module.exports.chooseParty = function(memcache, socket) {
-  // Information needed from memcache:
-  // - Current round
-  var currentRound = 1; //<-------------------From Memcache
-  // - Number of players in game
-  var numPlayers = 5; //<----------------------From Memcache
-  // - Previous party leader
-  var prevPartyLeader = 'player1'; //<----------------------From Memcache
-
-  // TODO: Set the current phase in the game to 'PARTY' in memcache
-  // TODO: Determine new party leader based on previous party leader
-  var currentPartyLeader = 'player1';  //<-------------------From Memcache
   
-  // Write logic to determine the number of players the party leader must choose
-  // given the current round and total players in game
+  // Get current round
+  memcache.incrRound().then(function(currentRound) {
+    memcache.getPids().then(function(pidsList) {
+      var numPlayers = pidsList.length;
+      // Find previous party leader and change
 
-  // Doesn't look like there's an apparent pattern to the number of players and round
-  // number to the number of party members so will hard code in a matrix for now:
-  var teamBuilder = [
-    // Each array is for number of players, while each element in the array is the
-    // current round:
-    // 5 Players:
-    [2, 3, 2, 3, 3],
-    // 6 Players:
-    [2, 3, 4, 3, 4],
-    // 7 Players:
-    [2, 3, 3, 4, 4],
-    // 8 Players:
-    [3, 4, 4, 5, 5],
-    // 9 Players:
-    [3, 4, 4, 5, 5],
-    // 10 Players:
-    [3, 4, 4, 5, 5]
-  ];
+      // prevLeader should be the ID of the previous leader [string]
+      memcache.getLeader(function(prevLeader) {
+        var oldIdx = pidsList.indexof(prevLeader);
+        var newIdx = pidsList[oldIdx + 1] ? oldIdx + 1 : 0;
+        var currentLeader = pidsList[newIdx];
 
-  var partySize = teamBuilder[numPlayers - 5][currentRound - 1];
-  
-  // TODO: Should signal through sockets to the players that the party is being chosen
-  // The party leader should be aware that he or she is the party leader and be given 
-  // the tools to choose the new party
-  socket.emit('sendParty', {
-  	gameId: 5318008,
-  	partySize,
-  	currentPartyLeader
+        memcache.setLeader(currentLeader);
+
+        // Doesn't look like there's an apparent pattern to the number of players and round
+        // number to the number of party members so will hard code in a matrix for now:
+        var teamBuilder = [
+          // Each array is for number of players, while each element in the array is the
+          // current round:
+          // 5 Players:
+          [2, 3, 2, 3, 3],
+          // 6 Players:
+          [2, 3, 4, 3, 4],
+          // 7 Players:
+          [2, 3, 3, 4, 4],
+          // 8 Players:
+          [3, 4, 4, 5, 5],
+          // 9 Players:
+          [3, 4, 4, 5, 5],
+          // 10 Players:
+          [3, 4, 4, 5, 5]
+        ];
+
+        var partySize = teamBuilder[numPlayers - 5][currentRound - 1];
+
+        // The current leader should be interpreted by the actual current leader on the
+        // client side to know that they should decide the party size based on the limit set
+        socket.emit('sendParty', {
+          gameId: 5318008,
+          partySize,
+          currentLeader
+        });
+
+        setTimeout(function() {
+          resolveParty(memcache, socket);
+        }, 5000);
+
+      });
+    });
   });
-  
-  // TODO: Set timer for resolveParty
-  setTimeout(function() {
-  	resolveParty(memcache, socket);
-  }, 5000);
+
 };
 
 var resolveParty = function(memcache, socket) {
-  // Information needed from memcache:
-  // - List of party members chosen by the party leader
-  var partyMembers = ['player1', 'player2']; //<-------------------------From Memcache
-  // - Current game phase 
-  var gamePhase = 'PARTY'; //<-----------From Memcache
-
-  // If phase from memcache is 'PARTY', then continue, otherwise, fizzle
-  if (gamePhase !== 'PARTY') {
-  	return;
-  }
   
-  // Retrieved the list of party members
+  // Get current phase to decide whether this function should run or fizzle
+  memcache.getTurnPhase(function(gamePhase) {
+    if (gamePhase !== 'PARTY') {
+      return;
+    }
 
-  // TODO: Set the new party into memcache
+    // TODO: If timer runs out and this function was called, there will not
+    // be enough party members so there should be a default sent
 
-  // Notify players of the current party
-  socket.emit('resolveParty', {
-  	gameId: 5318008,
-  	partyMembers,
+    // Party members should have been set by a previous socket. 
+    memcache.getTeam(function(partyMembers) {
+      socket.emit('resolveParty', {
+        gameId: 5318008,
+        partyMembers,
+      });
+
+      setTimeout(function() {
+        voteOnParty(memcache, socket);
+      }, 5000);
+
+    });
   });
 
-  // TODO: Set timer for voteOnParty
-  setTimeout(function() {
-  	voteOnParty(memcache, socket);
-  }, 5000);
 };
 
 module.exports.resolveParty = resolveParty;
