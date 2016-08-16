2573ac455d4c0a16c0014d7758419f559acd171b
cptran777 <cptran14@gmail.com>
2016-08-16 11:36:50 -0700

    camera controls implemented. Pending issue where camera doesn't start on scene

--- client/src/scripts/game.js
+++ client/src/scripts/game.js
@@ -1,6 +1,6 @@
-import THREE.OrbitControls from './orbit';
-import orientationInit from '.orientation';
-import THREE.StereoEffect from './stereoscopic.js';
+import OrbitControls from './orbit';
+import orientationInit from './orientation';
+import StereoEffect from './stereoscopic.js';
 
 export default {
   init: function () {
@@ -18,6 +18,7 @@ export default {
       //defaultColor: 0xffce00
       defaultColor: 0x00b8ff
     };
+    var self = this;
 
     //SET UP SCENE////////////////
 
@@ -33,6 +34,17 @@ export default {
     // Init scene
     this.scene = new THREE.Scene();
 
+    // Init renderer
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(this.WIDTH, this.HEIGHT);
+
+    this.element = this.renderer.domElement;
+    $gameContainer.append(this.element);
+
+    // Init stereo effect that will allow the game to be rendered in a stereoscopic
+    // view. Render with effect instead of renderer in render loop to get the view
+    this.effect = new StereoEffect(this.renderer);
+
     // Init camera
     this.camera = new THREE.PerspectiveCamera(
         VIEW_ANGLE, ASPECT, NEAR, FAR
@@ -40,19 +52,66 @@ export default {
     this.camera.position.z = 500;
     this.scene.add(this.camera);
 
-    // Init renderer
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(this.WIDTH, this.HEIGHT);
+    // Camera VR controls: 
 
-    this.element = this.renderer.domElement);
-    $gameContainer.append(this.element);
+    this.controls = new OrbitControls(this.camera, this.element);
+    this.controls.target.set(
+      this.camera.position.x + 0.15,
+      this.camera.position.y,
+      this.camera.position.z
+    );
+    this.controls.noPan = true;
+    this.controls.noZoom = true;
 
-    // Init stereo effect that will allow the game to be rendered in a stereoscopic
-    // view. Render with effect instead of renderer in render loop to get the view
-    this.effect = new THREE.StereoEffect(this.renderer);
+    // setOrientationControls will only activate if the user is on mobile. Otherwise,
+    // no effect
+    function setOrientationControls(e) {
+      if (!e.alpha) {
+        return;
+      }
+      self.controls = new THREE.DeviceOrientationControls(this.camera, true);
+      self.controls.connect();
+      self.controls.update();
+      self.element.addEventListener('click', fullscreen, false);
+      window.removeEventListener('deviceorientation', setOrientationControls, true);
+    }
+
+    window.addEventListener('deviceorientation', setOrientationControls, true);
+
+    // Will allow the container to become full screen
+    function fullscreen() {
+      if ($gameContainer.requestFullscreen) {
+        $gameContainer.requestFullscreen();
+      } else if ($gameContainer.msRequestFullscreen) {
+        $gameContainer.msRequestFullscreen();
+      } else if ($gameContainer.mozRequestFullScreen) {
+        $gameContainer.mozRequestFullScreen();
+      } else if ($gameContainer.webkitRequestFullscreen) {
+        $gameContainer.webkitRequestFullscreen();
+      }
+      resize();
+    }
+
+    function resize() {
+      var nativePixelRatio = window.devicePixelRatio = window.devicePixelRatio ||
+      Math.round(window.screen.availWidth / document.documentElement.clientWidth);
+
+      var devicePixelRatio = nativePixelRatio;
+      var width = window.innerWidth;// container.offsetWidth;
+      var height = window.innerHeight; // container.offsetHeight;
+      self.camera.aspect = devicePixelRatio;
+      self.camera.updateProjectionMatrix();
+      self.renderer.setSize(width, height);
+      self.effect.setSize(width, height);
+    }
+
+    function update() {
+      resize();
+      self.camera.updateProjectionMatrix();
+      self.controls.update();
+    }
     
     this.textureLoader = new THREE.TextureLoader();
-
 
     this.raycaster = new THREE.Raycaster();
     this.camMouse = {
@@ -62,20 +121,20 @@ export default {
     this.mouse = new THREE.Vector2();
 
     //MAIN DOCUMENT LISTENERS/////////////////////
-    document.addEventListener('mousemove', (e) => {
-      //console.log('{', e.clientX, e.clientY, '}');
-      this.mouse.x = (e.clientX / this.WIDTH) * 2 - 1;
-      this.mouse.y = - (e.clientY / this.HEIGHT) * 2 + 1;
-      this.camMouse.x = (e.clientX - this.WIDTH / 2);
-      this.camMouse.y = (e.clientY - this.HEIGHT / 2);
-    }, false);
+    // document.addEventListener('mousemove', (e) => {
+    //   //console.log('{', e.clientX, e.clientY, '}');
+    //   this.mouse.x = (e.clientX / this.WIDTH) * 2 - 1;
+    //   this.mouse.y = - (e.clientY / this.HEIGHT) * 2 + 1;
+    //   this.camMouse.x = (e.clientX - this.WIDTH / 2);
+    //   this.camMouse.y = (e.clientY - this.HEIGHT / 2);
+    // }, false);
 
-    window.addEventListener('resize', ()=> {
-      this.camera.aspect = window.innerWidth / window.innerHeight;
-      this.camera.updateProjectionMatrix();
+    // window.addEventListener('resize', ()=> {
+    //   this.camera.aspect = window.innerWidth / window.innerHeight;
+    //   this.camera.updateProjectionMatrix();
 
-      this.renderer.setSize( window.innerWidth, window.innerHeight );
-    }, false );
+    //   this.renderer.setSize( window.innerWidth, window.innerHeight );
+    // }, false );
 
 /*  //SKY BOX///////////////////////////////////
     //Todo: Convert to tga format, speedier loadup vs png
@@ -108,6 +167,8 @@ export default {
       let d = new Date();
       pointLight.position.x += 30 * Math.sin(Math.floor(d.getTime() / 10) * 0.02);
       pointLight.position.y += 20 * Math.sin(Math.floor(d.getTime() / 10) * 0.01);
+      update();
+      // this.effect.render(this.scene, this.camera);
       this.renderer.render(this.scene, this.camera);
 
       let numPlayers = this.players.length;
@@ -119,13 +180,15 @@ export default {
           playerObj.position.x += 2;
         }
       }
-    this.camera.position.x += (this.camMouse.x - this.camera.position.x) * 0.05;
-    this.camera.position.y += ( - this.camMouse.y - this.camera.position.y) * 0.05;
-    this.camera.lookAt(this.scene.position);
+      // this.camera.position.x += (this.camMouse.x - this.camera.position.x) * 0.05;
+      // this.camera.position.y += ( - this.camMouse.y - this.camera.position.y) * 0.05;
+      // this.camera.lookAt(this.scene.position);
+
     };
     render();
   },
   addPlayer: function(uid, color, role) {
+    console.log('add Player is running');
     if (this.players.length <= 10) {
       this.players.push({
         uid,
