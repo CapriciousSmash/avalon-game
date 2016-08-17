5da56a28a79015509e1a6a34b10f937b13f4bb17
andrew-heath <aheath42@gmail.com>
2016-08-17 15:17:04 -0700

    Implement basic async/promise test

--- test/redis-test.js
+++ test/redis-test.js
@@ -10,15 +10,20 @@ describe('Testing redis', function() {
     testCache = new redisDb(15);
   })
 
-  it('true should be true', function() {
+  it('should be able to set and get', function() {
     // do things
-    var testFunc = function() {
-      return true;
-    }
-    expect(testFunc()).to.equal(true);
+    var test = '5';
+    return testCache.data.setAsync('test', test)
+    .then(function() {
+      return testCache.data.getAsync('test');
+    })
+    .then(function(result) {
+      expect(result).to.equal(test);
+    })
   })
 
   afterEach(function() {
+    testCache.clear();
     testCache.data.quit();
   })
 
