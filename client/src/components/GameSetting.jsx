430258522a0d7f5f3031416efd287f528b428b83
Hien Tran <aznketchup@yahoo.com>
2016-08-20 16:48:08 -0700

    Add capacity drop down option for room.

--- client/src/components/GameSetting.jsx
+++ client/src/components/GameSetting.jsx
@@ -7,42 +7,59 @@ export default class GameSetting extends React.Component {
 
   componentWillMount() {
     var socket = this.props.socket;
-    socket.on('receiveCheckBox', function(person, value) {
+    socket.on('updateCharacter', function(person, value) {
       document.getElementById(person).checked = value;
     });
-    socket.on('sendUpdate', function(person, value) {
-      document.getElementById(person).checked = value;
+    socket.on('updateParty', function(value) {
+      document.getElementById('capacity').innerHTML = value;
     });
-
   }
 
   componentDidMount() {
     var socket = this.props.socket;
-    socket.emit('update', 'merlin');
-    socket.emit('update', 'assassin');
-
+    socket.emit('updateOnCharacter', 'merlin');
+    socket.emit('updateOnCharacter', 'assassin');
+    socket.emit('updateOnParty', 'capacity');
     if (this.props.gm !== this.props.currentUser) {
       document.getElementById('merlin').disabled = true;
       document.getElementById('assassin').disabled = true;
     }
   }
 
-  clickOnUserSide(e) {
+  pickSpecialCharacter(e) {
     var socket = this.props.socket;
     socket.emit('sendCheckBox', e.target.id);
   }
 
+  pickPartyMembers(e) {
+    var socket = this.props.socket;
+    socket.emit('capacity', e.target.value)
+  }
+
+  click
   render() {
     return (
       <div>
         <label>
-          <input type='checkbox' id='merlin' onClick={this.clickOnUserSide.bind(this)}></input>
+          <input type='checkbox' id='merlin' onClick={this.pickSpecialCharacter.bind(this)}></input>
           Merlin
         </label>
         <label> 
-          <input type='checkbox' id='assassin' onClick={this.clickOnUserSide.bind(this)}></input>
+          <input type='checkbox' id='assassin' onClick={this.pickSpecialCharacter.bind(this)}></input>
           Assassin
         </label>
+        <div className='dropdown'>
+          <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
+            Party Members
+            <span className="caret"></span>
+          </button>
+          <ul className='dropdown-menu'>
+            {[5,6,7,8,9,10].map(value =>
+              <li key={value} value={value} onClick={this.pickPartyMembers.bind(this)}>{value}</li>
+            )}
+          </ul>
+          <div id='capacity'></div>  
+        </div>
       </div>
     )
   }
