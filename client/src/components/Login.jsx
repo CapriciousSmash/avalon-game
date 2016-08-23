c67a236e9757926f137e0fcedd3272843c91c282
1kindjune <ejw322@nyu.edu>
2016-08-22 19:12:17 -0700

    Style the login/signup page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,26 +3,41 @@ import React from 'react';
 export default class SignIn extends React.Component {
   render() {
     return (
-      <div className="loginsignup">
-        <form className="text-center" action="/signin" method="post">
-          <fieldset>
-            <h3>Username </h3>
-            <input name="username" placeholder='Username'></input>
-            <h3>Password </h3>
-            <input name="password" type='password' placeholder='Password'></input><br />
-            <button type="submit" value="Submit">Submit</button>
-          </fieldset>
+      <div className="container loginSignupContainer">
+        <h3 className="sectionTitle">LOGIN</h3>
+        <form className="form-horizontal" action="/signin" method="post">
+            <div className="form-group">
+              <label for="username1" className="col-sm-2 col-sm-offset-1 control-label">Username</label>
+              <div className="col-sm-7">
+                <input id="username1" type="text" className="form-control" placeholder="Username"/>
+              </div>
+            </div>
+            <div className="form-group">
+              <label for="password2" className="col-sm-2 col-sm-offset-1 control-label">Password</label>
+              <div className="col-sm-7">
+                <input type="password" className="form-control" id="password2" placeholder="Password"/>
+              </div>
+            </div>
+            <button className="btn" type="submit" value="Submit">S u b m i t</button>
         </form>
-        <form className="text-center" action="/signup" method="post">
-          <fieldset>
-            <h3>Username </h3>
-            <input placeholder='Username'></input>
-            <h3>Password </h3>
-            <input type='password' placeholder='Password'></input><br />
-            <button type="submit" value="signup">Sign Up</button>
-          </fieldset>
+        <br />
+        <h3 className="sectionTitle">SIGNUP</h3>
+        <form className="form-horizontal" action="/signup" method="post">
+            <div className="form-group">
+              <label for="username1" className="col-sm-2 col-sm-offset-1 control-label">Username</label>
+              <div className="col-sm-7">
+                <input id="username1" type="text" className="form-control" placeholder="Username"/>
+              </div>
+            </div>
+            <div className="form-group">
+              <label for="password2" className="col-sm-2 col-sm-offset-1 control-label">Password</label>
+              <div className="col-sm-7">
+                <input type="password" className="form-control" id="password2" placeholder="Password"/>
+              </div>
+            </div>
+            <button className="btn" type="submit" value="Submit">S u b m i t</button>
         </form>
       </div>
-    )
+    );
   }
 }
