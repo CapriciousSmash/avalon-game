32ce8bca2a829fa6d9a3ca579eb093cfa1c2d646
andrew-heath <aheath42@gmail.com>
2016-08-23 11:26:54 -0700

    Organize actions and reducers for logging in and out

--- client/src/actions/login.js
+++ client/src/actions/login.js
@@ -3,9 +3,13 @@ import { LOGGED_IN, LOGGED_OUT } from './actionTypes';
 export default function login(user){
   return{
     type: LOGGED_IN,
-    uid: user,
-    settings: {
-      username: 'cat'
-    }
+    uid: user.id,
+    username: user.username
+  }
+}
+
+export default function logout(){
+  return{
+    type: LOGGED_OUT
   }
 }
--- client/src/reducers/auth.jsx
+++ client/src/reducers/auth.jsx
@@ -1,7 +1,9 @@
 import { LOGGED_IN, LOGGED_OUT } from '../actions/actionTypes';
 
 const initialState = {
-  authenticated: false
+  authenticated: false,
+  uid: null,
+  username: null
 };
 
 export default function auth (state = initialState, action) {
@@ -10,11 +12,15 @@ export default function auth (state = initialState, action) {
       return {
         ...state,
         authenticated: true
+        uid: action.uid,
+        username: action.username
       };
     case LOGGED_OUT:
       return {
         ...state,
-        authenticated: false  
+        authenticated: false,
+        uid: null,
+        username: null
       };
     default:
       return state;
