d2fa898ed38131409e59bbedce8a344f2f84d75f
cptran777 <cptran14@gmail.com>
2016-08-25 23:28:23 -0700

    Info Text content filled

--- client/src/components/GameRules.jsx
+++ client/src/components/GameRules.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import InfoText from './InfoText';
 
 const GameObjective = (props) => {
   return (
   	<div className="GameObjective">
   	  <h3>Objective:</h3>
-  	  <p>Avalon is a game of hidden loyalties. Players are either Loyal Servants
-  	  of Arthur fighting for Goodness and honor or aligned with the evil ways of
-  	  Mordred. Good wins the game by successfully completing three Quests. Evil 
-  	  wins if three Quests end in failure. Evil can also win by assassinating Merlin
-  	  at the game's end or if a Quest cannot be undertaken.</p>
-  	  <p>Players may make any claims during the game, at any point in the game. 
-  	  Discussion, deception, accusation, and logical deducation are all equally
-  	  important in order for Good to prevail or Evil to rule the day. </p>
+  	  <p>{InfoText.rulesObjectivePart1}</p>
+  	  <p>{InfoText.rulesObjectivePart2}</p>
+      <h3>Game Play:</h3>
+      <p>{InfoText.rulesGamePlayPart1}</p>
+      <p>{InfoText.rulesGamePlayPart2}</p>
+      <h3>Team Building:</h3>
+      <p>{InfoText.rulesTeamBuildPart1}</p>
+      <h3>Team Vote:</h3>
+      <p>{InfoText.rulesTeamVotePart1}</p>
+      <h3>Questing:</h3>
+      <p>{InfoText.rulesQuestPart1}</p>
+      <h3>Game End:</h3>
+      <p>{InfoText.rulesEndPart1}</p>
+      <p>{InfoText.rulesEndPart2}</p>
   	</div>
   );
 }
