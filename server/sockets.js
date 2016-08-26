195920d350fe7df6ff293fe7d7f70057bb391ec7
cptran777 <cptran14@gmail.com>
2016-08-26 12:09:19 -0700

    import modules to fix game start bug.

--- server/sockets.js
+++ server/sockets.js
@@ -3,6 +3,10 @@ var deepSearch = require('./utility/deepSearch.js');
 var memcache = require('./utility/makeServers.js').memcache;
 var lobbyState = require('./utility/makeServers.js').lobbyState;
 var players = require('./utility/makeServers.js').players;
+// Import the game logic router to allow calling of game logic functions
+// based on received signals
+var game = require('./logic/logic-main').gameLogic;
+var logicFilter = require('./logic/logic-intervene');
 
 module.exports = function(server) {
   var io = sockets.listen(server);
