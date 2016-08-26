4e43055617e88626eef06c064b1dbdb07b39486d
andrew-heath <aheath42@gmail.com>
2016-08-25 17:07:53 -0700

    Condense route functions, fix resend error on catch all

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,10 @@ var pgHelp = require('./db/controller/index.js');
 
 module.exports = function(app, passport) {
 
+  var sendFile = function(req, res) {
+    res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
+  };
+
   // utility endpoint to return player data for use in the app
   app.get('/stats', function(req, res) {
     // return information based on who's logged in
@@ -33,25 +37,13 @@ module.exports = function(app, passport) {
   });
 
   // These routes need auth, effectively middleware to catch-all route
-  app.get('/play', isAuth, function(req, res) {
-    res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
-  });
+  app.get('/play', isAuth);//, sendFile);
+  app.get('/profile', isAuth, sendFile);
+  app.get('/game', isAuth, sendFile);
 
-  app.get('/profile', isAuth, function(req, res) {
-    res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
-  });
-
-  app.get('/game', isAuth, function(req, res) {
-    res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
-  });
-
-  app.get('/gameinfo', function(req, res) {
-    res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
-  });
-
-  app.get('/signin', function(req, res) {
-    res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
-  });
+  // These routes don't need auth
+  app.get('/gameinfo', sendFile);
+  app.get('/signin', sendFile);
 
   // post request handlers for signing in and signing up
   app.post('/signin', passport.authenticate('local-login', {
@@ -67,7 +59,7 @@ module.exports = function(app, passport) {
   // catchall redirect to landing page
   app.get('*',function(req, res) {
     res.redirect('/');
-    res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
+    // sendFile(req, res);
   });
 
 };
